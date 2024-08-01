a92ec8359cebf8c87466754d19cca69c76e11bf1
JoelL77 <joellasalas77@gmail.com>
2024-08-01 10:11:24 -0300

    app piano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-piano';
+
+  aplicarSonido(numero: number): void {
+    const audio = new Audio();
+    audio.src = '/sonidos/note' + numero + '.mp3';
+    audio.load();
+    audio.play();
+  }
 }
